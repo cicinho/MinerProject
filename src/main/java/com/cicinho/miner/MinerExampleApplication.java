71529fffc1f79fef098f4edfacb4a8583a05d888
cicero <cicerojprn@gmail.com>
2018-10-02 19:52:47 -0300

    Inclusão da instanciação do PrivateNetworkMiner e solicitação da inicialização do miner

--- src/main/java/com/cicinho/miner/MinerExampleApplication.java
+++ src/main/java/com/cicinho/miner/MinerExampleApplication.java
@@ -3,10 +3,14 @@ package com.cicinho.miner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cicinho.miner.mining.PrivateNetworkMiner;
+
 @SpringBootApplication
 public class MinerExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinerExampleApplication.class, args);
+		
+		new PrivateNetworkMiner().initMiner();
 	}
 }
