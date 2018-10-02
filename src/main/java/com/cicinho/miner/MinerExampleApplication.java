a9139b92b501836cddf05d718209712cbe276784
Cicero Alves <cicerojprn@gmail.com>
2018-10-02 15:28:49 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/cicinho/miner/MinerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.cicinho.miner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinerExampleApplication.class, args);
+	}
+}
