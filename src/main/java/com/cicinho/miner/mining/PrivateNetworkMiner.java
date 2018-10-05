7b4bbca4e9243eacdf0fde61f8ae44e50efb020e
Cicero Alves <cicerojprn@gmail.com>
2018-10-05 16:15:55 -0300

    Modificação para configuração do nó por meio do application.properties

--- src/main/java/com/cicinho/miner/mining/PrivateNetworkMiner.java
+++ src/main/java/com/cicinho/miner/mining/PrivateNetworkMiner.java
@@ -1,9 +1,13 @@
 package com.cicinho.miner.mining;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.ethereum.config.SystemProperties;
 import org.ethereum.facade.EthereumFactory;
 import org.ethereum.samples.BasicSample;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import com.cicinho.miner.node.MinerNode;
 import com.typesafe.config.ConfigFactory;
@@ -11,29 +15,32 @@ import com.typesafe.config.ConfigFactory;
 public class PrivateNetworkMiner {
 
 	private static String logger = "Miner1";
-	private static String externalIp = "198.168.1.5";
+	
+	static Properties properties;
 
 	private static class MinerConfig {
 
 		private final String config =
 				// no need for discovery in that small network
-				"peer.discovery.enabled = false \n" + "peer.listen.port = 30335 \n" +
+				"peer.discovery.enabled = " + properties.getProperty("peer.discovery.enabled") + " \n" + 
+						"peer.listen.port = " + properties.getProperty("peer.listen.port") + " \n" +
 				// need to have different nodeId's for the peers
-						"peer.privateKey = 6ef8da380c27cea8fdf7448340ea99e8e2268fc2950d79ed47cbf6f85dc977ec \n" +
+						"peer.privateKey = " + properties.getProperty("peer.privateKey") + " \n" +
 						// our private net ID
-						"peer.networkId = 15 \n" +
+						"peer.networkId = " + properties.getProperty("peer.networkId") + " \n" +
 						// we have no peers to sync with
-						"sync.enabled = false \n" +
+						"sync.enabled = " + properties.getProperty("sync.enabled") + " \n" +
 						// genesis with a lower initial difficulty and some predefined known funded
 						// accounts
-						"genesis = genesis.json \n" +
+						"genesis = " + properties.getProperty("genesis") + " \n" +
 						// two peers need to have separate database dirs
-						"database.dir = database \n" +
+						"database.dir = " + properties.getProperty("database.dir") + " \n" +
 						// when more than 1 miner exist on the network extraData helps to identify the
 						// block creator
-						"mine.extraDataHex = cccccccccccccccccccc \n" + "mine.cpuMineThreads = 2 \n"
-						+ "cache.flush.blocks = 1 \n"
-						+ "peer.discovery.external.ip = " + externalIp;
+						"mine.extraDataHex = " + properties.getProperty("mine.extraDataHex") + " \n" + 
+						"mine.cpuMineThreads = " + properties.getProperty("mine.cpuMineThreads") + " \n"
+						+ "cache.flush.blocks = " + properties.getProperty("cache.flush.blocks") + " \n"
+						+ "peer.discovery.external.ip = " + properties.getProperty("peer.discovery.external.ip");
 
 		@Bean
 		public MinerNode node() {
@@ -59,7 +66,10 @@ public class PrivateNetworkMiner {
 					+ "Mb) to generate DAG for mining (DAG requires min 1G). For this sample it is recommended to set -Xmx2G JVM option");
 			return;
 		}
-
+		try {
+			properties = PropertiesLoaderUtils.loadAllProperties("application.properties");
+		} catch (IOException e) {			
+		}
 		BasicSample.sLogger.info("Starting EthtereumJ miner instance!");
 		EthereumFactory.createEthereum(MinerConfig.class);
 	}
